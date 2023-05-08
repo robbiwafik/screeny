2e21f716a743e6e58c929dbce6985b6282eccb89
Robbi <codewithwafik@gmail.com>
2023-05-09 06:20:23 +0700

    Initial commit

---
+++ src/main/java/com/company/screeny/ScreenyApplication.java
@@ -0,0 +1,12 @@
+package com.company.screeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/screeny/ScreenyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.screeny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
